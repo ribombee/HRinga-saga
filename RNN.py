cad32727e5b928bc30f247c322516216a78ae4ba
ribombee <margretl16@ru.is>
2018-04-03 00:14:18 +0000

    Reading several sagas now the default function

--- RNN.py
+++ RNN.py
@@ -1,12 +1,12 @@
 import tensorflow as tf
 import numpy as np
+import glob
 
-num_epochs = 100            # The number of times we iterate through the training data
+num_epochs = 1000            # The number of times we iterate through the training data
 learning_rate = 0.001       # The size of the steps we take in the gradient descent
-batch_size = 200            # How many characters we feed through before back-propagating all of them
-sequence_len = 20           # The length of the sequence of characters used to predict the next one
+batch_size = 100            # How many characters we feed through before back-propagating all of them
+sequence_len = 10           # The length of the sequence of characters used to predict the next one
 num_cells = 80              # The number of hidden units per LSTM cell
-num_lstms = 1               # Currently not in use.  TODO: implement
 forget = 1.0                # The forget bias of LSTM forget gates.
 model_name = "default_RNN"  # The name of the network as it gets saved
 
@@ -16,9 +16,12 @@ def read_files():
 
     :return:
     """
-    njala = open(file='Islendingasogur/HranaSagaHrings.txt', encoding='UTF-8')
-    # Todo: read more sagas in and concatenate them
-    return njala.read()
+    all_sagas = ''
+
+    for single_file in glob.glob('Islendingasogur/*'):
+        all_sagas += open(file = single_file, encoding = 'UTF-8').read()
+
+    return all_sagas
 
 
 def embed(sagas):
@@ -152,13 +155,11 @@ def activate_clean(output):
     # We start by getting all positive numbers by running the values through an activation function.
 
     # Activate
-    #output = sigmoid(output / 0.5)
     output = np.exp(output / 0.5) #Just plugging the value of the temperature in for now
     # Normalize
     output = output / sum(output)
     # Choose a character
     index_of_probability = np.random.choice(range(len(unique_characters)), p = output)
-    #index_of_probability = np.argmax(output)
     clean_output = np.zeros(len(output))
     clean_output[index_of_probability] = 1
 
@@ -178,9 +179,10 @@ if __name__ == '__main__':
 
     # Parameters for back propagation
     cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=pred, labels=output_p))  # Our cost function
-    # We will use gradient descent to find a local minimum of the cost function.
 
-    optimizer = tf.train.RMSPropOptimizer(learning_rate=learning_rate).minimize(cost)
+    # We will use gradient descent to find a local minimum of the cost function.
+    # Through some experiments we found that AdamOptimizer gave us the fastest convergence.
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
     # Setting variables for other programs to reference when generating strings on a saved model
     tf.get_variable('unique_characters', dtype=tf.string, initializer=tf.convert_to_tensor(unique_characters))
@@ -202,15 +204,17 @@ if __name__ == '__main__':
         # Instantiate a saver for the model
         saver = tf.train.Saver()
 
-        #for i in range(len(seed)):
-            #user_onehots[0][i][unique_characters.index(seed[i])] = 1
-
         for j in range(num_epochs):
 
             # Train our model
             acc, loss = train(optimizer, input_p, output_p, accuracy)
+
             # Test it by making it generate some characters
-            generate(input_data[110:111:])
+            #Uncomment the line below to get a generated string after each training epoch.
+            #generate(input_data[110:111:])
             # Save our model so we can use it later
             saver.save(sess, ('./model/' + model_name))
-            print("Accuracy: ", acc, ", Loss: ", loss)
+            #Print some useful statistics
+            print(j, ",",acc, ",", loss)
+            #print("Accuracy: ", acc, ", Loss: ", loss)
+        generate(input_data[111:112:])
